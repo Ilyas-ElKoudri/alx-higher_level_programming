9f08a33765123b3d3c510f0ad850f923fb99d109
Ilyas-ElKoudri <125877228+Ilyas-ElKoudri@users.noreply.github.com>
2024-01-20 15:37:58 +0100

    Delete 0x02-python-import_modules directory

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import sys
-if __name__ == "__main__":
-    argc = len(sys.argv) - 1
-    count = 0
-    for i in range(argc):
-        count += int(sys.argv[i + 1])
-    print(count)
