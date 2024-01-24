c0be7b800419be548c3a18372bb7a644468026ee
Ilyas-Elkoudri <ilyasskodri@gmail.com>
2024-01-24 06:21:13 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
-
 if __name__ == "__main__":
     names = dir(hidden_4)
-    valid_names = [name for name in names if not name.startswith('__')]
-    for name in sorted(valid_names):
-        print(name)
+    for i in names:
+        if i[0] != '_':
+            print(i)
